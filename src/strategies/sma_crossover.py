2bd66b9c72343bd5625f9b5f00e7b3535c6f84a3
Yousif Aluobaidy <alyousif@Yousifs-MacBook-Pro.local>
2026-03-27 11:22:06 -0400

    Removed Regime filter from SMA crossover to test the profit and loss without the filter

--- src/strategies/sma_crossover.py
+++ src/strategies/sma_crossover.py
@@ -1,5 +1,4 @@
 from indicators.sma import sma
-from filters.regime import regime
 def sma_crossover(d, fast_period, slow_period):
     fast_sma = sma('Close',fast_period,d)
     slow_sma = sma('Close', slow_period,d)
@@ -11,16 +10,14 @@ def sma_crossover(d, fast_period, slow_period):
     
     signals = [0]
     
-    regime_arr = regime(d, column_name=slow, lookback=10, threshold=0.05) 
-    
     for i in range(1, len(d)):
         regimen = regime_arr[i]
         if d[fast].iloc[i] is None or d[slow].iloc[i] is None:
             signals.append(0)
-        if ((d[fast].iloc[i-1] <= d[slow].iloc[i-1] and d[fast].iloc[i] > d[slow].iloc[i]) and (regimen == 1 or regimen == 0)):
+        if ((d[fast].iloc[i-1] <= d[slow].iloc[i-1] and d[fast].iloc[i] > d[slow].iloc[i])):
             #Buy signal
             signals.append(1)
-        elif ((d[fast].iloc[i-1] >= d[slow].iloc[i-1] and d[fast].iloc[i] < d[slow].iloc[i]) and (regimen == -1 or regimen == 0)):
+        elif ((d[fast].iloc[i-1] >= d[slow].iloc[i-1] and d[fast].iloc[i] < d[slow].iloc[i])):
             # Sell signal
             signals.append(-1)
         else:
