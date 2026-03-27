2d9c88e85ac9479c84e6ca0a10392ecabafaa3ef
Yousif Aluobaidy <alyousif@Yousifs-MacBook-Pro.local>
2026-03-27 11:17:32 -0400

    imported os to in download data file to ensure streamlit creates a new directory if none is found

--- src/data_processes/download_data.py
+++ src/data_processes/download_data.py
@@ -1,7 +1,12 @@
 import yfinance as yf
+import os
 
-def download_data(symbol,start_date, end_date):
-    #Fetch data from Yahoo Finance
-    data = yf.download(symbol,start=start_date,end= end_date)
-    #Store data as a CSV file
+def download_data(symbol, start_date, end_date):
+    # Fetch data from Yahoo Finance
+    data = yf.download(symbol, start=start_date, end=end_date)
+    
+    # Ensure the 'data' directory exists before attempting to save
+    os.makedirs("data", exist_ok=True)
+    
+    # Store data as a CSV file
     data.to_csv(f"data/{symbol}_{start_date[:4]}_{end_date[:4]}_data.csv")
